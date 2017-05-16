932325166f08167c1ebcf28d9cfc00b89b96fa93
Vital Teresh <vster2006@gmail.com>
2017-05-16 16:45:38 +0300

    add serpent, sha256, sha512

---
+++ testing/sha256-main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <opencl/sha256.h>
+#include <opencl/encoder.h>
+// #include <opencl/filters.h>
+
+int main(int argc, char* argv[])
+   {
+   if(argc < 2)
+      {
+      std::cout << "Usage: sha256 <filenames>" << std::endl;
+      return 1;
+      }
+
+   OpenCL::Pipe pipe(new OpenCL::HashFilter<OpenCL::SHA2_256>,
+                     new OpenCL::HexEncoder);
+
+   for(int j = 1; argv[j] != 0; j++)
+      {
+      std::ifstream file(argv[j]);
+      if(!file)
+         {
+         std::cout << "ERROR: could not open " << argv[j] << std::endl;
+         continue;
+         }
+      file >> pipe;
+      file.close();
+      pipe.close();
+      std::cout << pipe << "  " << argv[j] << std::endl;
+      }
+   return 0;
+   }
+
