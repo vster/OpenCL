e2e0d343945a69801267805bddaf13c8ae950fe6
Vital Teresh <vster2006@gmail.com>
2017-05-17 16:13:45 +0300

    mod des: add test2

--- testing/des-main.cpp
+++ testing/des-main.cpp
@@ -21,6 +21,11 @@ byte kzu1[KL] = {0x01,0x23,0x45,0x67,0x89,0xAB,0xCD,0xEF};
 byte to1[BS] =  {0x01,0x23,0x45,0x67,0x89,0xAB,0xCD,0xE7};
 byte tc1[BS] =  {0xC9,0x57,0x44,0x25,0x6A,0x5E,0xD3,0x1D};
 
+byte kzu2[KL] = {0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x80};
+byte to2[BS] =  {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+byte tc2[BS] =  {0x9C,0xC6,0x2D,0xF4,0x3B,0x6E,0xED,0x74};
+
+
 int main(void)
 {
     OpenCL::DES d1;
@@ -32,7 +37,7 @@ int main(void)
 
     // for (i = 0; i < KL; i++)
     //    key[i] = i;
-    memcpy(key,kzu1,KL);
+    memcpy(key,kzu2,KL);
 
     cout << "DES" << endl;
     cout << "KL = " << KL << endl;
@@ -42,7 +47,7 @@ int main(void)
 
     // for (i = 0; i < BS; i++)
     //     data[i] = i + 0x30;
-    memcpy(data,to1,BS);
+    memcpy(data,to2,BS);
 
     cout << "Initial data" << endl;
     output_hex(data, BS);
@@ -54,7 +59,7 @@ int main(void)
     cout << "Encrypted data" << endl;
     output_hex(datac, BS);
 
-    if (memcmp(datac,tc1,BS))
+    if (memcmp(datac,tc2,BS))
         cout << "Encrypt test failed." << endl;
     else
         cout << "Encrypt test is OK." << endl;
